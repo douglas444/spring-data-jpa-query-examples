7f394c74fce00ca233056ebeb2c3f7dd5fde26e9
Douglas Monteiro <douglas.m.cavalcanti@gmail.com>
2025-01-28 09:01:13 -0300

    remove customer specification class

--- src/main/java/dev/cavalcanti/store/service/CustomerService_SpringSpecification.java
+++ src/main/java/dev/cavalcanti/store/service/CustomerService_SpringSpecification.java
@@ -1,8 +1,9 @@
 package dev.cavalcanti.store.service;
 
 import dev.cavalcanti.store.entity.Customer;
+import dev.cavalcanti.store.entity.Order;
 import dev.cavalcanti.store.repository.CustomerRepository;
-import dev.cavalcanti.store.specification.CustomerSpecification;
+import jakarta.persistence.criteria.Join;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +12,9 @@ import java.util.List;
 @Service
 public class CustomerService_SpringSpecification implements CustomerService {
 
-    private final CustomerSpecification customerSpecification;
     private final CustomerRepository customerRepository;
 
-    public CustomerService_SpringSpecification(CustomerSpecification customerSpecification,
-                                               CustomerRepository customerRepository) {
-        this.customerSpecification = customerSpecification;
+    public CustomerService_SpringSpecification(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 
@@ -24,9 +22,21 @@ public class CustomerService_SpringSpecification implements CustomerService {
     public List<Customer> whoBought(String productCode) {
 
         Specification<Customer> specification = Specification
-                .where(this.customerSpecification.containsOrderWithProductCode(productCode))
-                .and(this.customerSpecification.isActive());
+                .where(containsOrderWithProductCode(productCode))
+                .and(isActive());
 
         return this.customerRepository.findAll(specification);
     }
+
+    private Specification<Customer> isActive() {
+        return (root, query, builder) -> builder.equal(root.get("active"), true);
+    }
+
+    private Specification<Customer> containsOrderWithProductCode(String productCode) {
+        return (root, query, builder) -> {
+            Join<Customer, Order> join = root.join("orders");
+            return builder.equal(join.get("product").get("code"), productCode);
+        };
+    }
+
 }
